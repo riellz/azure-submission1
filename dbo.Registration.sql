f8eefe6431d8b7278d9026ad0ad59683a27b55ae
riellz <34902773+riellz@users.noreply.github.com>
2020-01-18 13:23:44 +0700

    Add files via upload

---
+++ dbo.Registration.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Registration] (
+    [Id]    INT           NOT NULL,
+    [name]  NVARCHAR (50) NOT NULL,
+    [email] NVARCHAR (50) NOT NULL,
+    [job]   NVARCHAR (50) NOT NULL,
+    [date]  DATE          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
